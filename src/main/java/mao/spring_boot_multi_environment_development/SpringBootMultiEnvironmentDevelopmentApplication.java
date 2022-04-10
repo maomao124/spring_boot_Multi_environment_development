096dbee6a8e003889979856a35a1af311eaf9db0
毛 <1296193245@qq.com>
2022-04-10 15:21:16 +0800

    spring_boot_Multi_environment_development

---
+++ src/main/java/mao/spring_boot_multi_environment_development/SpringBootMultiEnvironmentDevelopmentApplication.java
@@ -0,0 +1,15 @@
+package mao.spring_boot_multi_environment_development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiEnvironmentDevelopmentApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootMultiEnvironmentDevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/spring_boot_multi_environment_development/SpringBootMultiEnvironmentDevelopmentApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.spring_boot_multi_environment_development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultiEnvironmentDevelopmentApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
